04e59659882236e4df617cc2b9a021f8737530fb
Alinia Andersson <alinia93@gmail.com>
2024-07-05 22:35:50 +0200

    hello

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -5,14 +5,18 @@ using MauiBlazor3.Platform.Android;
 
 namespace OneMoreDate
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", 
+       MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             WebViewSoftInputPatch.Initialize();
+            RequestedOrientation = ScreenOrientation.Portrait;
         }
+
+       
 
     }
 }
