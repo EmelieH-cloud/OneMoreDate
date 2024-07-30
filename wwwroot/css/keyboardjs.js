a237927612b409fe22e7e4d93e5d941abb9f6965
R Ann-Marie Göransson <Ann-Marie@MacBook-Air.lan>
2024-07-30 15:17:09 +0200

    ios

---
+++ wwwroot/css/keyboardjs.js
@@ -0,0 +1,13 @@
+﻿function adjustForKeyboard() {
+    window.addEventListener('resize', function () {
+        if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') {
+            // Justera höjden på sidan när tangentbordet visas
+            document.body.style.height = (window.innerHeight + 'px');
+        } else {
+            // Återställ höjden på sidan när tangentbordet göms
+            document.body.style.height = '100vh';
+        }
+    });
+}
+
+
