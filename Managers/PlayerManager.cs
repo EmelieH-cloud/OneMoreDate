713528b8c46e5eff36405270d143161606a18e14
emeliehensfelt <emelie046@gmail.com>
2024-08-02 22:00:07 +0200

    löst saker

--- Managers/PlayerManager.cs
+++ Managers/PlayerManager.cs
@@ -5,12 +5,9 @@ namespace OneMoreDate.Managers
     public static class PlayerManager
     {
         public static List<Player> Players { get; set; } = new();
-   
+
         public static int PlayerCount { get; set; }
 
-      
-
-  
 
         public static void AddActivePlayers(List<string> players)
         {
@@ -31,7 +28,7 @@ namespace OneMoreDate.Managers
 
         public static void ClearAllPlayerHands()
         {
-            for(int i = 0; i<Players.Count; i++)
+            for (int i = 0; i < Players.Count; i++)
             {
                 if (Players[i].Hand.Count > 0)
                 {
